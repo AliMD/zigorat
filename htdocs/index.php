0cc4023ad1d03f728148c01ec6dcb07366e5823f
alimd <ali@mihandoost.com>
2012-07-23 10:25:31 +0430

    debug mode

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,6 +1,12 @@
 <?php
 
-$page =isset($_GET[page])?$_GET[page]:'home';
+$sitedown = true;
+
+session_start();
+
+isset($_GET['debug']) and $_SESSION['debug']=$_GET['debug'];
+
+$page = ($sitedown && !$_SESSION['debug']) ? 'underdev' : (isset($_GET[page]) ? $_GET[page] : 'home');
 
 @file_exists("./$page.php") or $page='err/index';
 
