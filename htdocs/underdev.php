a89785ce680c2a62b5b6df7a7b47ff748c505006
alimd <ali@mihandoost.com>
2012-07-23 09:59:20 +0430

    add under devs

---
+++ htdocs/underdev.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8" />
+	<title>..::Zigorat::..</title>
+	<link rel="stylesheet/less" type="text/css" href="underdev.less" />
+	<script src="scripts/less.js" type="text/javascript"></script>
+	<script type="text/javascript" src="scripts/html5shiv.js"></script>
+	<script type="text/javascript"> document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>'); </script>
+	<script type="text/javascript" src="scripts/underdev.js"></script>
+</head>
+</head>
+<body>
+	<header></header>
+	<section class='slideshow'>
+		<div class="logo">
+			<h1 class="indent">Zigorat</h1>
+		</div>
+		<div class="noise"></div>
+		<section class="backimg">
+			<script type="text/javascript">
+				for(var i=1;i<7;i++) document.write('<div style="background-image :url(images/gallery/home'+i+'.jpg);"></div>');
+			</script>
+		</section>
+	</section>
+	<footer>
+		<h1>Under Construction</h1>
+	</footer>
+</body>
+</html>
