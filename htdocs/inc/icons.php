74634b90ad6c6a4351a3910e666b39a147876cf8
alimd <ali@mihandoost.com>
2012-07-23 09:38:41 +0430

    icon modul

---
+++ htdocs/inc/icons.php
@@ -0,0 +1,45 @@
+<section class="links">
+	<ul>
+		<li class="icon" style="top:0">
+			<a href="#" >
+				<img src="images/logo1.jpg" />
+			</a>
+		</li>
+		<li class="icon" style="top:20px">
+			<a href="#" >
+				<img src="images/logo2.png" />
+			</a>
+		</li>
+		<li class="icon" style="top:40px">
+			<a href="#" >
+				<img src="images/logo3.jpg" />
+			</a>
+		</li>
+		<li class="icon" style="top:60px">
+			<a href="#" >
+				<img src="images/logo4.jpg" />
+			</a>
+		</li>
+		<li class="icon" style="top:80px">
+			<a href="#" >
+				<img src="images/logo5.jpg" />
+			</a>
+		</li>
+		<li class="icon" style="top:100px">
+			<a href="#" >
+				<img src="images/logo6.jpg" />
+			</a>
+		</li>
+		<li class="icon" style="top:120px">
+			<a href="#" >
+				<img src="images/logo7.jpg" />
+			</a>
+		</li>
+		<li class="icon" style="top:140px">
+			<a href="#" >
+				<img src="images/logo8.png" />
+			</a>
+		</li>
+	</ul>
+	<div class="clear"></div>
+</section>
