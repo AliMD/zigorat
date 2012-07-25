06dc0a88c5f261902ca79fbe9bcc58f876482381
Ali Mihandoost <i@ali.md>
2012-07-25 22:13:12 +0430

    add img alt !!!

--- htdocs/inc/icons.php
+++ htdocs/inc/icons.php
@@ -2,42 +2,42 @@
 	<ul>
 		<li class="icon" style="top:0">
 			<a href="#" >
-				<img src="images/logo1.jpg" />
+				<img alt="icon 1" src="images/logo1.jpg" />
 			</a>
 		</li>
 		<li class="icon" style="top:20px">
 			<a href="#" >
-				<img src="images/logo2.png" />
+				<img alt="icon 2" src="images/logo2.png" />
 			</a>
 		</li>
 		<li class="icon" style="top:40px">
 			<a href="#" >
-				<img src="images/logo3.jpg" />
+				<img alt="icon 3" src="images/logo3.jpg" />
 			</a>
 		</li>
 		<li class="icon" style="top:60px">
 			<a href="#" >
-				<img src="images/logo4.jpg" />
+				<img alt="icon 4" src="images/logo4.jpg" />
 			</a>
 		</li>
 		<li class="icon" style="top:80px">
 			<a href="#" >
-				<img src="images/logo5.jpg" />
+				<img alt="icon 5" src="images/logo5.jpg" />
 			</a>
 		</li>
 		<li class="icon" style="top:100px">
 			<a href="#" >
-				<img src="images/logo6.jpg" />
+				<img alt="icon 6" src="images/logo6.jpg" />
 			</a>
 		</li>
 		<li class="icon" style="top:120px">
 			<a href="#" >
-				<img src="images/logo7.jpg" />
+				<img alt="icon 7" src="images/logo7.jpg" />
 			</a>
 		</li>
 		<li class="icon" style="top:140px">
 			<a href="#" >
-				<img src="images/logo8.png" />
+				<img alt="icon 8" src="images/logo8.png" />
 			</a>
 		</li>
 	</ul>
