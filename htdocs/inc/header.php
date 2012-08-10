4216c2516c4316062a10a7013eccbfc2cffec4cf
alimd <ali@mihandoost.com>
2012-08-11 02:13:37 +0430

    add 1utils

--- htdocs/inc/header.php
+++ htdocs/inc/header.php
@@ -7,6 +7,7 @@
 	<script type="text/javascript" src="scripts/html5shiv.js"></script>
 	<script type="text/javascript" src="scripts/less.js"></script>
 	<script type="text/javascript">document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');</script>
+	<script type="text/javascript" src="scripts/1utils.js" ></script>
 	<script type="text/javascript" src="scripts/script.js" ></script>
 </head>
 <body>
