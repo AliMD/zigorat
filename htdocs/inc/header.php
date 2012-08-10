f9db5688e3c1e29e90b871378a436dff073ba668
alimd <ali@mihandoost.com>
2012-08-11 01:49:31 +0430

    page title

--- htdocs/inc/header.php
+++ htdocs/inc/header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
-	<meta charset="utf-8">
-	<title>..:: Zigorat ::..</title>
+	<meta charset="utf-8" />
+	<title>Zigorat | <?php echo $title ?></title>
 	<link rel="stylesheet/less" type="text/css" href="style.less" />
 	<script type="text/javascript" src="scripts/html5shiv.js"></script>
 	<script type="text/javascript" src="scripts/less.js"></script>
 	<script type="text/javascript">document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');</script>
-	<script type="text/javascript" src="scripts/script.js" ></script>	
+	<script type="text/javascript" src="scripts/script.js" ></script>
 </head>
 <body>
--- htdocs/index.php
+++ htdocs/index.php
@@ -9,6 +9,8 @@ isset($_GET['debug']) and $_SESSION['debug']=$_GET['debug'];
 
 $page = ($sitedown && !$_SESSION['debug']) ? 'underdev' : (isset($_GET[page]) ? $_GET[page] : 'home');
 
+$title = ucwords(str_replace(array('-','%20'), ' ', $page));
+
 @file_exists("./$page.php") or $page='err/index';
 
 @include "./$page.php";
