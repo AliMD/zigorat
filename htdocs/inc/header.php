f8b08e1c041f1a722129625c2033dd1420eca542
alimd <ali@mihandoost.com>
2012-08-11 06:06:21 +0430

    fav icon

--- htdocs/inc/header.php
+++ htdocs/inc/header.php
@@ -3,6 +3,8 @@
 <head>
 	<meta charset="utf-8" />
 	<title>Zigorat | <?php echo $title ?></title>
+	<link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
+	<link rel="apple-touch-icon-precomposed" type="image/png" href="images/icon.png" /> 
 	<link rel="stylesheet/less" type="text/css" href="style.less" />
 	<script type="text/javascript" src="scripts/html5shiv.js"></script>
 	<script type="text/javascript" src="scripts/less.js"></script>
