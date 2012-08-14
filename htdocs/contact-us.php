b3c0b08be2c812a86bdec9428d2269152a78ba7e
AliGH <aghasizadeh70@gmail.com>
2012-08-14 23:01:15 +0430

    cut contact js in script.js

--- htdocs/contact-us.php
+++ htdocs/contact-us.php
@@ -1,52 +1,5 @@
 <?php include 'inc/header.php'; ?>
 <script type="text/javascript">
-	window.onload = function(){
-		
-		var myform = document.forms.item(0);
-		
-		myform.onsubmit = function(){
-			
-			var ret = true;
-			
-			for(var i=0;i<myform.elements.length;i++){
-				if(myform.elements.item(i).className!='ok'){
-					ret = false;
-					break;
-				}
-			}
-			
-			return ret;
-		}
-		
-		for (var i=0;i<myform.elements.length;i++){
-			
-			var inp = myform.elements.item(i);
-			
-			if (inp.type!='text' && inp.type!='textarea'){
-				continue;
-			}
-			
-			inp.onfocus = function(){
-				if (this.value == this.defaultValue){
-					this.value = '';
-				}
-			}
-	
-			inp.onblur = function(){
-				
-				if (this.value!=''){
-					this.className='ok';
-				}else{
-					this.className='nok'
-				}
-				
-				if (this.value == ''){
-					this.value = this.defaultValue;
-				}
-				
-			}
-		}
-	}
 </script>
 <body>
 <section class="container contact">
