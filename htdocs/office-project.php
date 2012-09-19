982f90df5ac72772ddd8685695bdf83c54e8318a
Ali Aghasizadeh <aghasizadeh70@gmail.com>
2012-09-19 18:28:16 +0330

    add other pages and fix names

---
+++ htdocs/office-project.php
@@ -0,0 +1,66 @@
+<?php include 'inc/header.php'; ?>
+<section class="container project">
+	<div class="logo"></div>
+	<section class="left-col left">
+		<?php include "inc/menus.php"; ?>
+	</section>
+<?php include "inc/projects-menu.php"; ?>
+		<div class="project_pics">
+			<div class="g1">
+				<div class="on_g ">
+					<div class="bgg-img1"></div>
+					<div id="ong"></div>
+					<a id="g1">Project name</a>
+					<a id="g11">
+						01
+						<br />
+						image
+					</a>
+				</div>
+			</div>
+			<section class="pprd-hover">
+				<div class="pprd1-hover"></div>
+			</section>
+
+			<div class="g2">
+				<div class="on_g ">
+					<div class="bgg-img2"></div>
+					<div id="ong"></div>
+					<a id="g1">Project name</a>
+					<a id="g11">
+						01
+						<br />
+						image
+					</a>
+				</div>
+			</div>
+			<section class="pprd-hover">
+				<div class="pprd2-hover"></div>
+			</section>
+
+			<div class="g3">
+				<div class="on_g ">
+					<div class="bgg-img3"></div>
+					<div id="ong"></div>
+					<a id="g1">Project name</a>
+					<a id="g11">
+						01
+						<br />
+						image
+					</a>
+				</div>
+			</div>
+			<section class="pprd-hover">
+				<div class="pprd3-hover"></div>
+			</section>
+
+		</div>
+		<div class="top-arrow">
+			<a href="#"></a>
+		</div>
+		<div class="bot-arrow">
+			<a href="#"></a>
+		</div>
+	</section>
+</section>
+<?php include 'inc/footer.php'; ?>
