47b0bd3a35728846c66d96776aca8872b43f88cc
Ali Aghasizadeh <aghasizadeh70@gmail.com>
2012-09-22 16:58:36 +0330

    remove junks

--- htdocs/projects.php
+++ htdocs/projects.php
@@ -2,11 +2,12 @@
 <section class="container">
 	<img id="transparentImg" src="images/img-map.gif" usemap="#mapPrdMenu" alt="transparent" width="500" height="718"  title="" /><!--b64 wont work here!!! (R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==)-->
 	<div id="prdHome">Product Home</div>
+	<div id="prdOffice">Product Home</div>
 	<map name="mapPrdMenu" id="mapPrdMenu">
-		<area id='prdHomeMap' shape="poly" coords="364, 10, 124, 148, 124, 280, 368, 138" href="#" alt="" title="" />
-		<area id='prdHomeMap' shape="poly" coords="315, 187, 72, 325, 73, 460, 316, 320" href="#" alt="" title="" />
+		<area id='prdShoppingMap' shape="poly" coords="364, 10, 124, 148, 124, 280, 368, 138" href="#" alt="" title="" />
+		<area id='prdExhibitionMap' shape="poly" coords="315, 187, 72, 325, 73, 460, 316, 320" href="#" alt="" title="" />
 		<area id='prdHomeMap' shape="poly" coords="276, 363, 32, 505, 34, 639, 276, 497" href="#" alt="" title="" />
-		<area id='prdHomeMap' shape="poly" coords="412, 433, 172, 573, 170, 709, 414, 565" href="#" alt="" title="" />
+		<area id='prdOfficeMap' shape="poly" coords="412, 433, 172, 573, 170, 709, 414, 565" href="#" alt="" title="" />
 	</map>
 	<section class="logo"></section>
 	<section class="left-col left">
@@ -22,9 +23,6 @@
 						<a class="pic1">shopping</a>
 					</div>
 				</div>
-				<section class="prd-hover">
-					<div class="prd1-hover"></div>
-				</section>
 				<div class="prd2">
 					<div class="rlt">
 						<div class="bg-img2"></div>
@@ -32,19 +30,13 @@
 						<a class="pic2">exhibition</a>
 					</div>
 				</div>
-				<section class="prd-hover">
-					<div class="prd2-hover"></div>
-				</section>
-				<div class="prd3">
+			<div class="prd3">
 					<div class="rlt">
 						<div class="bg-img3"></div>
 						<div class="fg-img"></div>
 						<a class="pic3">home</a>
 					</div>
 				</div>
-				<section class="prd-hover">
-					<div class="prd3-hover"></div>
-				</section>
 				<div class="prd4">
 					<div class="rlt">
 						<div class="bg-img4"></div>
@@ -52,9 +44,6 @@
 						<a class="pic4">office</a>
 					</div>
 				</div>
-				<section class="prd-hover">
-					<div class="prd4-hover"></div>
-				</section>
 			</section>
 		</section>
 	</section>
