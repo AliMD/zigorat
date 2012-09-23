8df789a18d49f9b5632eb08d8daf8ef495b03327
Ali Aghasizadeh <aghasizadeh70@gmail.com>
2012-09-23 09:46:07 +0330

    add in inc file

---
+++ htdocs/inc/imagemap-gallery.php
@@ -0,0 +1,6 @@
+<img id="transparentGalleryImg" src="data:images/gif;base64,R0lGODlhAQABAIAAAP///////yH5BAEHAAEALAAAAAABAAEAAAICTAEAOw==" usemap="#mapGallery" alt="transparent" width="750" height="390"  title="" />
+<map name="mapGallery" id="mapGallery">
+	<area id='Gallery0Map' shape="poly" coords="242, 48, 0, 189, 2, 379, 242, 239" href="#" alt="" title="" />
+	<area id='Gallery01Map' shape="poly" coords="487, 51, 246, 189, 246, 379, 487, 237" href="#" alt="" title="" />
+	<area id='Gallery02Map' shape="poly" coords="733, 48, 491, 189, 491, 379, 731, 242" href="#" alt="" title="" />
+</map>
