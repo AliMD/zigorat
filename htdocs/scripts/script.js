a373be4be7611e0830739d0576dd1faeb9f1d93b
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-10 16:07:09 +0330

    add slideshow script

--- htdocs/scripts/script.js
+++ htdocs/scripts/script.js
@@ -134,6 +134,71 @@ pagejs = function(pg){
 		imageMapLink('#Gallery02', './Gallery02')
 	}
 
+	else if(page=='fullscreen'){
+
+		var	ease = !!window.Zepto ? 'ease-out' : 'swing';
+		$.extend($.fn,{
+			fadeLoop :function(options){
+
+				options=$.extend({
+					duration : 2500,
+					freez : 1500,
+					delay : 10,
+					startIndex : 0,
+					fadeFirstImage : true,
+					zIndex : -3,
+					zIndexAct : -2
+				},options);
+
+				options.startIndex--;
+
+				var nextPic,
+					pics    =this,
+					indx    =options.startIndex,
+					plen    =this.length,
+					fadeIn  ={opacity:1},
+					fadeOut ={opacity:0};
+
+				var nextPic = function(){
+					pics.eq(indx).animate(fadeOut,options.duration,ease,function(){
+						 $(this).css({'z-index':options.zIndex});
+
+					});
+					indx=indx<plen-1?indx+1:0;
+					setTimeout(function(){
+						pics.eq(indx).css({'z-index':options.zIndexAct}).animate(fadeIn,options.duration,ease,function(){
+							setTimeout(nextPic,options.freez);
+						});
+					},options.delay+10);
+				};
+
+				pics.css(fadeOut).css({'z-index':options.zIndex});
+
+				if(!options.fadeFirstImage){
+					pics.eq(0).css(fadeIn).css({'z-index':options.zIndexAct});
+					indx++;
+					setTimeout(nextPic,options.freez);
+				}else{
+					nextPic();
+				}
+			}
+		});
+
+		$(function(){
+			$('.fullimg > div').fadeLoop({
+				delay : 300,
+				freez : 4500,
+				duration : 2000,
+				zIndex : -3,
+				zIndexAct : -2,
+				startIndex : 0,
+				fadeFirstImage : false
+			});
+		});
+
+		(window.Zepto || window.jQuery);
+	}
+
 	else if(page=='contact-us'){
 			function validateText(str,len){
 			return str.length >= len;
