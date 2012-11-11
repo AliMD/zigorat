1b6fe292e9f90b82aa1d71f63e2a2109bcc91fbf
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-11 10:03:56 +0330

    fix js for fullscreen and home page

--- htdocs/scripts/script.js
+++ htdocs/scripts/script.js
@@ -110,6 +110,13 @@ pagejs = function(pg){
 			tmp=tmp.concat([event.offsetX,event.offsetY])
 			console.log(tmp);
 		})
+
+		$('.fullimg > div').fadeLoop({
+				delay : 300,
+				freez : 4000,
+				duration : 2000,
+				fadeFirstImage : false
+		});
 	}
 
 	else if(page == 'projects') {
@@ -136,67 +143,12 @@ pagejs = function(pg){
 
 	else if(page=='fullscreen'){
 
-		var	ease = !!window.Zepto ? 'ease-out' : 'swing';
-		$.extend($.fn,{
-			fadeLoop :function(options){
-
-				options=$.extend({
-					duration : 2500,
-					freez : 1500,
-					delay : 10,
-					startIndex : 0,
-					fadeFirstImage : true,
-					zIndex : -3,
-					zIndexAct : -2
-				},options);
-
-				options.startIndex--;
-
-				var nextPic,
-					pics    =this,
-					indx    =options.startIndex,
-					plen    =this.length,
-					fadeIn  ={opacity:1},
-					fadeOut ={opacity:0};
-
-				var nextPic = function(){
-					pics.eq(indx).animate(fadeOut,options.duration,ease,function(){
-						 $(this).css({'z-index':options.zIndex});
-
-					});
-					indx=indx<plen-1?indx+1:0;
-					setTimeout(function(){
-						pics.eq(indx).css({'z-index':options.zIndexAct}).animate(fadeIn,options.duration,ease,function(){
-							setTimeout(nextPic,options.freez);
-						});
-					},options.delay+10);
-				};
-
-				pics.css(fadeOut).css({'z-index':options.zIndex});
-
-				if(!options.fadeFirstImage){
-					pics.eq(0).css(fadeIn).css({'z-index':options.zIndexAct});
-					indx++;
-					setTimeout(nextPic,options.freez);
-				}else{
-					nextPic();
-				}
-			}
-		});
-
-		$(function(){
 			$('.fullimg > div').fadeLoop({
 				delay : 300,
-				freez : 4500,
+				freez : 4000,
 				duration : 2000,
-				zIndex : -3,
-				zIndexAct : -2,
-				startIndex : 0,
 				fadeFirstImage : false
 			});
-		});
-
-		(window.Zepto || window.jQuery);
 	}
 
 	else if(page=='about-us'){
