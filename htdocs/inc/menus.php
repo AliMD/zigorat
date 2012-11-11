9f044a46c77b9cdf560182d79a0dbf037853a8fa
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-11 11:18:51 +0330

    remove link page

--- htdocs/inc/menus.php
+++ htdocs/inc/menus.php
@@ -1,6 +1,6 @@
 <?php
 	// "menu 1 title,menu 1 url;menu 2 title,menu 2 url;..."
-	$menus = "home,./;about us,./about-us;our customers,./our-customers;projects,./projects;contact us,./contact-us;links,./links";
+	$menus = "home,./;about us,./about-us;our customers,./our-customers;projects,./projects;contact us,./contact-us;";
 ?>
 
 <nav class="nav left ">
