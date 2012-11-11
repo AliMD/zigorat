fa95c6c86a20e0210b14d8081643360d0a5b7218
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-11 09:30:54 +0330

    remove len

--- htdocs/fullscreen.php
+++ htdocs/fullscreen.php
@@ -48,13 +48,11 @@
 			$images_path = 'images/fullscren-images/shopping';
 
 			$images = scandir($images_path);
-			$slidesLen = 0;
 			foreach($images as $img){
 				$img_arr = explode('.', $img);
 				$img_type = strtolower( end($img_arr) );
 				if ($img_type=='jpg') {
 					echo "<div style=\"background-image:url('$images_path/$img');\"></div>";
-					$slidesLen++;
 				}
 			}
 		?>
