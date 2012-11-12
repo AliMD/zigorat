d4753edc47a4c53f1df0ae4ac53810d4bd9275c6
Ali aGHasizadeh <aghasizadeh70@gmail.com>
2012-11-12 12:25:10 +0330

    change projects pages strategy

--- htdocs/projects-home.php
+++ htdocs/projects-home.php
@@ -6,49 +6,25 @@
 	</section>
 <?php include "inc/projects-menu.php"; ?>
 		<div class="project_pics">
+			<div class="left-panel">
 			<?php
-				include 'inc/imagemap-gallery.php';
+				for (i=0;i>3;i++){
 			?>
+					<section class='prd'>
+						<h1></h1>
+						<h2></h2>
+					</section>
+			<?php
+				}
+			 ?>
+			</div>
+			<div class="center-panel">
+				
+			</div>
+			<div class="right-panel">
+				
+			</div>
 
-			<div class="g1">
-				<div class="on_g ">
-					<div class="bgg-img1"></div>
-					<div id="Gallery0">
-						<a id="g1">Project name</a>
-						<a id="g11">
-							01
-							<br />
-							image
-						</a>
-					</div>
-				</div>
-			</div>
-			<div class="g2">
-				<div class="on_g">
-					<div class="bgg-img2"></div>
-					<div id="Gallery01">
-						<a id="g1">Project name</a>
-						<a id="g11">
-							01
-							<br />
-							image
-						</a>
-					</div>
-				</div>
-			</div>
-			<div class="g3">
-				<div class="on_g ">
-					<div class="bgg-img3"></div>
-					<div id="Gallery02">
-						<a id="g1">Project name</a>
-						<a id="g11">
-							01
-							<br />
-							image
-						</a>
-					</div>
-				</div>
-			</div>
 		</div>
 		<div class="top-arrow">
 			<a href="#"></a>
