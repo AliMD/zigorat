d54a491e555463fc9888da4791cf7d5fe978674f
Ali aGHasizadeh <me@aligh.pro>
2012-11-20 12:18:14 +0330

    use INC

--- htdocs/pages/fullscreen.php
+++ htdocs/pages/fullscreen.php
@@ -1,26 +1,4 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="utf-8" />
-	<title>Zigorat | <?php echo $title ?></title>
-	<link rel="shortcut icon" type="image/x-icon" href="./favicon.ico" />
-	<link rel="apple-touch-icon-precomposed" type="image/png" href="images/icon.png" /> 
-	<link rel="stylesheet/less" type="text/css" href="style.less" />
-	<script type="text/javascript" src="scripts/html5shiv.js"></script>
-	<script type="text/javascript" src="scripts/less.js"></script>
-	<script type="text/javascript">document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');</script>
-	<script type="text/javascript" src="scripts/script.js" ></script>
-	<script type="text/javascript">
-		$(function(){
-			try{
-				pagejs("<?php echo $page; ?>");
-			}catch(e){
-				console.log(e);
-			}
-		});
-	</script>
-</head>
-<body class="<?php echo $page; ?>">
+<?php include 'inc/header.php'; ?>
 <section class="fullscreen-container">
 	<section class="btns">
 		<ul class="info">
@@ -71,5 +49,4 @@
 		<div class="counter">01/06</div>
 	</section>
 </section>
-</body>
-</html>
+<?php include 'inc/footer.php'; ?>
