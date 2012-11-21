056da02ee950c0ccbe6f561696bafc4ce3bba871
Ali Mihandoost <i@ali.md>
2012-11-21 12:04:48 +0330

    dynamic products

--- htdocs/app/moduls.php
+++ htdocs/app/moduls.php
@@ -2,12 +2,7 @@
 
 session_start();
 
-function inc($filename,$folder='inc'){
-	global $template;
-	@include "$folder/$filename.php";
-}
-
-inc('db','app');
+require_once('app/db.php');
 
 function get_page(){
 	return isset($_GET[page]) ? strtolower($_GET[page]) : 'home';
@@ -21,8 +16,50 @@ function showunder(){
 	}
 }
 
+function inc($filename,$folder='inc'){
+	global $template;
+	@include "$folder/$filename.php";
+}
+
 function finalise (){
 	db_close();
 }
 
+function gen_project($project,$linkclass){
+	$html = '';
 
+	$project['img'] = array();
+	$project['imglinks']='';
+	$project['imglen']=0;
+
+	for($i=1;$i<=10;$i++){
+		if(strlen($project["img$i"])>3){
+			$img = $project["img$i"];
+			$project['imglen']++;
+			$act = $i==1 ? ' act' : '';
+			$project['imglinks'] .= "<a class='links$act $linkclass' href='images/projects/$img' title='$project[excerpt_en]'></a>";
+		}
+	}
+
+	$html .= "
+				<section class='prd' style=\"background-image:url('images/projects/$project[thumb]');\">
+					<h1>$project[name_en]</h1>
+					<h2>
+						<span>$project[imglen]<br />images</span>
+					</h2>
+					$project[imglinks]
+				</section>
+			";
+	return $html;
+}
+
+function gen_projects_list($projects_arr){
+	$html = array('','','');
+	$len = sizeof($projects_arr);
+	for($i=0; $i<$len; $i+=3) {
+		for($j=0;$j<3;$j++){
+			$projects_arr[$i] and $html[$j] .= gen_project($projects_arr[$i+$j],"prdpic".($i+$j));
+		}
+	}
+	return $html;
+}
--- htdocs/pages/projects_list.php
+++ htdocs/pages/projects_list.php
@@ -4,57 +4,11 @@
 	<section class="left-col left">
 		<?php inc('menus'); ?>
 	</section>
-<?php inc('projects-menu'); ?>
+		<?php inc('projects-menu'); ?>
 		<div class="project_pics">
-			<div class="left-panel">
-			
-			<?php
-				for ($i=0;$i<3;$i++){
-			?>
-					<section class='prd' style="background-image:url('images/g1.png')">
-						<h1>project name</h1>
-						<h2>
-							<span>01<br />images</span>
-						</h2>
-					</section>
-			<?php
-				}
-			?>
-
-			</div>
-			<div class="center-panel">
-			
-			<?php
-				for ($i=0;$i<3;$i++){
-			?>
-					<section class='prd' style="background-image:url('images/g2.png')">
-						<h1>project name</h1>
-						<h2>
-							<span>01<br />images</span>
-						</h2>
-					</section>
-			<?php
-				}
-			?>	
-
-			</div>
-			<div class="right-panel">
-			
-			<?php
-				for ($i=0;$i<3;$i++){
-			?>
-					<section class='prd' style="background-image:url('images/g3.png')">
-						<h1>project name</h1>
-						<h2>
-							<span>01<br />images</span>
-						</h2>
-					</section>
-			<?php
-				}
-			?>
-
-			</div>
-
+			<div class="left-panel"><?php echo $template['projects'][0] ?></div>
+			<div class="center-panel"><?php echo $template['projects'][1] ?></div>
+			<div class="right-panel"><?php echo $template['projects'][2] ?></div>
 		</div>
 		<div class="top-arrow">
 			<a href="#"></a>
