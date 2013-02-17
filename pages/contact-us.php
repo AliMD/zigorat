ca9682c34173324c526f46d016e6e27425c839f9
Ali aGHasizadeh <me@aligh.pro>
2013-02-17 13:24:13 +0330

    fix contact

--- pages/contact-us.php
+++ pages/contact-us.php
@@ -7,37 +7,37 @@
 	<section class="right-col">
 		<section class="form-holder left">
 			<form action="sendmail" method="post" target='ifrm'>
-				<label class="label">your name : </label>
+				<label class="label">نام : </label>
 				<input type="text" id="name" name="name" /><div class="name"></div>
 				<br/>
-				<label class="label">email : </label>
+				<label class="label">پست الکترونیکی : </label>
 				<input type="text" id="email" name="email" /><div class="email"></div>
 				<br/>
-				<label class="label">tel : </label>
+				<label class="label">تلفن : </label>
 				<input type="text" id="pNumber" name="pNumber" /><div class="pNumber"></div>
 				<br/>
-				<label class="label">subject : </label>
+				<label class="label">موضوع : </label>
 				<input type="text" id="subject" name="subject"  /><div class="subject"></div>
 				<br/>
-				<label class="label ">comment : </label>
+				<label class="label ">متن پیام : </label>
 				<textarea name="txt" id="txt"></textarea><div class="txt"></div>
-				<input type="submit" class="ok" name="submit" value="submit"/>
+				<input type="submit" class="ok" name="submit" value="ارسال"/>
 			</form>
 			<iframe id='ifrm' name='ifrm' src="" frameborder="0" scrolling="no"></iframe>
 		</section>
 		<section class="info">
 			<div class="info-pic1"></div>
 			<div class="desc">
-				Unit 1,No.86,Felestin 7 Ave,
+				مشهد، میدان فلسطین
 				<br/>
-				Felestine Blvd, Mashhad, Iran
+				فلسطین 7، پلاک 86
 			</div>
 			<br/>
 			<div class="info-pic1"></div>
 			<div class="desc">
-				Tel: +98 511 840 9894
+				+98 511 840 9894 : تلفن
 				<br/>
-				Fax: +98 511 842 1139
+				+98 511 842 1139 : فکس
 			</div>
 			<br/>
 			<div class="info-pic1"></div>
@@ -47,7 +47,7 @@
 				<a href="#">info@zigorat.co</a>
 			</div>
 			<div class="info-pic2">
-				<a href="#" >employment form</a>
+				<a href="#" >فرم استخدام</a>
 			</div>
 			<div class="clear"></div>
 		</section>
