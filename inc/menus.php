3d11ed90cdb615d7be8c4ffb87db4116569f9bb4
Ali aGHasizadeh <me@aligh.pro>
2013-02-17 13:06:02 +0330

    Change menu

--- inc/menus.php
+++ inc/menus.php
@@ -1,6 +1,6 @@
 <?php
 	// "menu 1 title,menu 1 url;menu 2 title,menu 2 url;..."
-	$menus = "home,./;about us,./about-us;our customers,./our-customers;projects,./projects;contact us,./contact-us;";
+	$menus = "خانه,./;درباره ما,./about-us;مشتریان,./our-customers;پروژه ها,./projects;ارتباط با ما,./contact-us;";
 ?>
 
 <nav class="nav left ">
