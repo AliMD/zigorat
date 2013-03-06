3d6a5b8607bb9e46453303302664a816a034e211
Ali aGHasizadeh <me@aligh.pro>
2013-03-06 15:33:36 +0330

    Add ie9

--- inc/header.php
+++ inc/header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>		<html lang="en-US" class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>			<html lang="en-US" class="lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>			<html lang="en-US" class="lt-ie9"> <![endif]-->
+<!--[if lt IE 7]>		<html lang="en-US" class="lt-ie10 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>			<html lang="en-US" class="lt-ie10 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>			<html lang="en-US" class="lt-ie9 lt-ie10"> <![endif]-->
+<!--[if IE 9]>			<html lang="en-US" class="lt-ie10"> <![endif]-->
 <!--[if gt IE 8]><!-->	<html lang="en-US" class="no-ie"> <!--<![endif]-->
 <head>
 	<meta charset="utf-8" />
