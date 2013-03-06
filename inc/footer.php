244a926000fd9ee88963488cbe826046fe397752
Ali aGHasizadeh <me@aligh.pro>
2013-03-06 13:25:04 +0330

    Fix zepto for iEs

--- inc/footer.php
+++ inc/footer.php
@@ -1,7 +1,7 @@
 <?php //console_log_show(); ?>
 <?php include inc('analytic'); ?>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
-<script type="text/javascript" src="scripts/zepto.js" ></script>
+<script>if('__proto__' in {}) document.write('<script src="scripts/zepto.js"><\/script>');</script>
 <script type="text/javascript" src="scripts/colorbox.js" ></script>
 <script type="text/javascript" src="scripts/script.js" ></script>
 <script type="text/javascript">
