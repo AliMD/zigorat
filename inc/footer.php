a3b720e4b6b84c7024af0529bcdbf6a324ee459e
Mohammad Reza Shariatzadeh <mrshcom@gmail.com>
2013-03-07 10:17:22 +0330

    Add liquid zooming link!

--- inc/footer.php
+++ inc/footer.php
@@ -4,8 +4,9 @@
 <script type="text/javascript" src="scripts/zepto.js" ></script>
 <script type="text/javascript" src="scripts/colorbox.js" ></script>
 <script type="text/javascript" src="scripts/script.js" ></script>
+<script type="text/javascript" src="scripts/liquid_zoom.js"></script>
 <script type="text/javascript">
-	pagejs("<?php echo $template['page']; ?>");
+		pagejs("<?php echo $template['page']; ?>");
 </script>
 <img src="images/background.jpg" onload="pageonload()" width="0" height="0" alt="zigorat background preloader" class="hidden" />
 </body>
