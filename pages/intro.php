9c692721fa6b1140aadf2f7ccf89a11484c08ac7
Mohammad Reza Shariatzadeh <mrshcom@gmail.com>
2013-04-17 11:50:57 +0430

    intro page

--- pages/intro.php
+++ pages/intro.php
@@ -1,7 +1,12 @@
 <?php inc('header'); ?>
 
 <section class="container intro">
-	<p>JUST TEST</p>
+	<div class="fa">
+		<a href="home_fa"></a>
+	</div>
+	<div class="en">
+		<a href="./"></a>
+	</div>
 </section>
 
 <?php inc('footer'); ?>
